41f62a478376ec50d595e5e9c957abca3940c331
Allyson Walpole <allyson@cs.utexas.edu>
2017-11-07 18:25:32 -0600

    idk what to add, so here's everything

---
+++ R3/src/myPackage/MyClass.java
@@ -0,0 +1,44 @@
+package myPackage;
+
+import java.util.ArrayList;
+
+import p.actions.*;
+
+public class MyClass {
+	public void myMethod() {
+		///////////////////////////////////////////////////////////////////////
+		//
+		//	Write your script here
+		//Create a new class named “D”.
+		  RPackage pkg = RProject.getPackage("RefactoringCrawler", "edu.uiuc.detectRefactorings.detection");
+		  RClassOrInterface vis = pkg.newClass("LikelinessVisitor");
+		  RField instance = vis.addSingleton();
+		  RClassOrInterface fd =  pkg.getClass("FieldDetection");
+		  String[] sig = {"computeLikeliness", "edu.uiuc.detectRefactorings.util.Node", "edu.uiuc.detectRefactorings.util.Node"};
+		  RMethod comp = fd.getMethod(sig);
+		  ArrayList<RMethod> relatives = comp.getRelatives();
+		  for(RMethod rel : relatives)
+		  {
+			  rel.addParameter(vis, instance.toString());
+		  }
+		  for(RMethod rel : relatives)
+		  {
+			  RParameter del = rel.getParameter(2);
+			  rel.moveAndDelegate(del);
+		  }
+		  String[] sig2 = {"computeLikeliness", "edu.uiuc.detectRefactorings.util.Node", "edu.uiuc.detectRefactorings.util.Node","edu.uiuc.detectRefactorings.detection.LikelinessVisitor" };
+		  comp = fd.getMethod(sig2);
+		  relatives = comp.getRelatives();
+		  for(RMethod rel : relatives)
+		  {
+			  rel.rename("accept");
+		  }
+		  ArrayList<RMethod> visitingMethods = vis.getAllMethods();
+		  for(RMethod visitM : visitingMethods)
+		  {
+			  visitM.rename("visit");
+		  }
+		//
+		///////////////////////////////////////////////////////////////////////
+	}
+}
---
+++ eclipse/jongwook.java
@@ -0,0 +1,4 @@
+package ID_0;
+
+public class ID_1 {
+}
